dcd16a7ac70199ea601abdec90db4e7cb395ec56
Tiago Nunes <tiago.nunes.lx@gmail.com>
2020-09-07 15:08:55 +0100

    ProdutoComTudo

--- app/src/main/java/com/example/gestaodacozinha/ui/produtos/ProdutoDetalhesViewModel.kt
+++ app/src/main/java/com/example/gestaodacozinha/ui/produtos/ProdutoDetalhesViewModel.kt
@@ -2,8 +2,6 @@ package com.example.gestaodacozinha.ui.produtos
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.example.gestaodacozinha.data.Produto
 import com.example.gestaodacozinha.data.ProdutosDao
 
@@ -13,12 +11,6 @@ class ProdutoDetalhesViewModel(
     application: Application
 ) : AndroidViewModel(application) {
 
-    private val _produto = MutableLiveData<Produto>()
-    val produto: LiveData<Produto>
-        get() = _produto
-
-    init {
-        _produto.value = produto
-    }
+    val produtoComTudo = database.obterProdutoComTudo(produto.id)
 
 }
