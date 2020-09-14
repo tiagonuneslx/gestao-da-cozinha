565dd6e63af6c4687a425b486b0ba3700aca39ba
Tiago Nunes <tiago.nunes.lx@gmail.com>
2020-09-14 20:49:42 +0100

    Toast ao clicar apagar

--- app/src/main/java/com/example/gestaodacozinha/utils/ApagarUtil.kt
+++ app/src/main/java/com/example/gestaodacozinha/utils/ApagarUtil.kt
@@ -3,6 +3,7 @@ package com.example.gestaodacozinha.utils
 import android.content.Context
 import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.example.gestaodacozinha.R
 
@@ -10,6 +11,7 @@ fun alternarCorApagar(context: Context, imgView: ImageView, apagar: Boolean) {
     if (apagar) {
         val colorInt: Int = ContextCompat.getColor(context, R.color.apagarAtivado)
         imgView.imageTintList = ColorStateList.valueOf(colorInt)
+        Toast.makeText(context, "Toque no item para apagar", Toast.LENGTH_SHORT).show()
     } else
         imgView.imageTintList = null
 }
