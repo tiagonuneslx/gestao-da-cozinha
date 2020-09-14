f7d445eeb7c8bfe956a24ba5292721f117b214cc
Tiago Nunes <tiago.nunes.lx@gmail.com>
2020-09-14 22:19:30 +0100

    Adicionar Categoria ao adicionar produto (alpha)

--- app/src/main/java/com/example/gestaodacozinha/ui/registos/produtos/AdicionarProdutoFragment.kt
+++ app/src/main/java/com/example/gestaodacozinha/ui/registos/produtos/AdicionarProdutoFragment.kt
@@ -2,16 +2,21 @@ package com.example.gestaodacozinha.ui.registos.produtos
 
 import android.Manifest
 import android.os.Bundle
+import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.example.gestaodacozinha.R
 import com.example.gestaodacozinha.databinding.AdicionarProdutoFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
+
 
 @AndroidEntryPoint
 class AdicionarProdutoFragment : Fragment() {
@@ -68,9 +73,34 @@ class AdicionarProdutoFragment : Fragment() {
             }
         }
 
+        val adicionarCategoriaDialog: AlertDialog = buildAdicionarCategoriaDialog()
+
+        binding.botaoAdicionarCategoria.setOnClickListener {
+            adicionarCategoriaDialog.show()
+        }
+
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
 
         return binding.root
+    }
+
+    private fun buildAdicionarCategoriaDialog(): AlertDialog {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.apply {
+            val input = EditText(requireContext())
+            input.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_CAP_SENTENCES
+            setView(input)
+            setPositiveButton(
+                R.string.ok
+            ) { dialog, id ->
+                Timber.d(input.toString())
+            }
+            setNegativeButton(
+                R.string.cancel
+            ) { dialog, id -> }
+            setTitle("Adicionar categoria")
+        }
+        return builder.create()
     }
 }
